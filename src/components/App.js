c5048b3947ffed118f9d091f51e8331bca806325
joseph <jbgomez88@gmail.com>
2019-02-05 11:49:20 -0800

    Implement Header component and add top margin to first station entry

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import StationListContainer from './../containers/StationListContainer';
 import PlayerContainer from './../containers/PlayerContainer';
+import Header from './Header';
 
 const App = () => (
   <React.Fragment>
+    <Header />
     <StationListContainer />
     <PlayerContainer />
   </React.Fragment>
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './../styles/Header.css';
+
+const Header = () => (
+  <div className="header fixed-top">
+    <h2>TuneMin</h2>
+  </div>
+);
+
+export default Header;
