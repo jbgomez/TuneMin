6b5a45ee80a3ab4b93e329cab2801e57eaf3b2ff
joseph <jbgomez88@gmail.com>
2019-02-05 11:52:52 -0800

    Add responsive classes to Player component for mobile browsers

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,14 +5,14 @@ const Player = ({ station, active }) => (
   active &&
   <div className="player fixed-bottom">
     <div className="row align-items-center">
-      <div className="col-1">
+      <div className="col-12 col-md-1 text-center text-md-left">
         <img className="img-fluid" src={station.imgUrl} />
       </div>
-      <div className="col-9 mt-3">
+      <div className="col-12 col-md-9 mt-3 mx-2 mx-md-0">
         <h6>{station.name}</h6>
         <p>{station.description}</p>
       </div>
-      <div className="col-2 text-center">
+      <div className="col-12 col-md-2 text-center">
         <audio controls src={station.streamUrl} />
       </div>
     </div>
